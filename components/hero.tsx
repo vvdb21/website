b89a9247a5425711fe7eca773aa59ce7897822c0
Viktor Van den Berghe <vvdb2006@gmail.com>
2026-05-01 16:24:59 +0200

    hero page changes

--- components/hero.tsx
+++ components/hero.tsx
@@ -42,7 +42,7 @@ export function Hero() {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ delay: 0.6 }}
-          className="mt-12"
+          className="mt-12 translate-y-[4cm]"
         >
           <motion.button
             onClick={() => {
